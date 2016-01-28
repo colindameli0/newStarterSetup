cb4a7933aac5026635b3874cfcc0ba1b2db8de01
colindameli0 <colin.damelio@gmail.com>
2016-01-28 12:25:58 -0500

    everybody's working for the updates

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,12 @@
-console.log("some fucking tests");
-console.log('I don\'t give a fuck');
-console.log('last test');
-console.log('FINALL TEST');
+$(document).ready(function() {
+
+    var babyMenu = $('a.menu-trigger');
+    var menu = $('nav ul');
+    //MOBILE MENU TOGGLING AND ICON TOGGLING
+    babyMenu.on('click', function(e){
+    	e.preventDefault();
+    	menu.slideToggle('800');
+    	$(this).find('i').toggleClass('fa-bars fa-times');
+    })
+
+});
